4ffc9362902426510cbd996cfb03a0a5922fc7f2
Ivan <belzee92@gmail.com>
2019-11-03 09:55:00 -0500

    fix categories seeds duplicated

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,6 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
     return [
-        'name' => $faker->randomElement(array ('Category 1','Category 2','Category 3'))
+        'name' => $faker->name
     ];
 });
