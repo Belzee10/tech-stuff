75d0cc2196f7e87c3001c04eb2493bf942b9e831
Ivan <belzee92@gmail.com>
2019-11-05 17:29:16 -0500

    add auth interceptor to http-client service

--- public/tech-stuff-front/src/services/http-client.js
+++ public/tech-stuff-front/src/services/http-client.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
 import { apiBaseUrl } from '../environment';
+import appLocalStorage from './storage';
 
 const config = {
   baseURL: apiBaseUrl
 };
 
 const httpClient = axios.create(config);
+
+/**
+ * Add interceptors to Axios
+ * @param {*} config
+ */
+const authInterceptor = config => {
+  const token = appLocalStorage.getItem('token');
+  config.headers.Authorization = token;
+  return config;
+};
+
+httpClient.interceptors.request.use(authInterceptor);
 
 export default httpClient;
