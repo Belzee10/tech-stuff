2a2817606748b86421b520dc5133790b340c7839
Ivan <belzee92@gmail.com>
2019-11-18 22:45:48 -0500

    create UserForm cmp

---
+++ public/tech-stuff-front/src/components/Users/UserForm/UserForm.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-form ref="form" v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-text-field
+            v-model="value.name"
+            class="name"
+            label="Name"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="value.lastName"
+            class="last-name"
+            label="Last Name"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="value.email"
+            class="email"
+            label="Email"
+            type="email"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="value.password"
+            class="password"
+            label="Password"
+            type="password"
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-select
+            v-model="value.role"
+            class="role"
+            :items="['member', 'admin']"
+            label="Role"
+          ></v-select>
+        </v-col>
+        <v-col cols="12">
+          <v-btn :disabled="!valid" small class="submit primary" @click="submit"
+            >Submit</v-btn
+          >
+          <v-btn small class="cancel secondary ml-2" @click="cancel"
+            >Cancel</v-btn
+          >
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  name: 'UserForm',
+  props: {
+    isEditable: {
+      type: Boolean,
+      default: false
+    },
+    populateWith: {
+      type: Object,
+      default: () => null
+    }
+  },
+  data: () => ({
+    valid: true,
+    value: {
+      name: '',
+      lastName: '',
+      email: '',
+      password: '',
+      role: ''
+    }
+  }),
+  methods: {
+    submit() {
+      if (this.$refs.form.validate()) this.$emit('submit', this.value);
+    },
+    cancel() {
+      this.$emit('cancel');
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ public/tech-stuff-front/src/components/Users/UserForm/__tests__/UserForm.spec.js
@@ -0,0 +1,23 @@
+import { createWrapper } from '@/test-utils/factories';
+import UserForm from '../UserForm.vue';
+
+describe('UserForm.vue', () => {
+  test('should emit a "submit" event with the correct payload', () => {
+    const wrapper = createWrapper(UserForm);
+    const formValue = {
+      name: 'name',
+      lastName: 'lastName',
+      email: 'email@email.com',
+      password: '123',
+      role: 'admin'
+    };
+    const button = wrapper.find('.v-btn.submit');
+    wrapper.find('.name input').setValue(formValue.name);
+    wrapper.find('.last-name input').setValue(formValue.lastName);
+    wrapper.find('.email input').setValue(formValue.email);
+    wrapper.find('.password input').setValue(formValue.password);
+    wrapper.find('.role input').setValue(formValue.role);
+    button.trigger('click');
+    expect(wrapper.emitted('submit')[0]).toEqual([formValue]);
+  });
+});
